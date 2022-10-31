c208a4a21108bd6c27ccc5f2376ca689f1d1fa41
ge-asp <94190704+ge-asp@users.noreply.github.com>
2022-10-31 14:46:12 +0200

    Tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Todotable from './components/Todolist';
 
 function App() {
+  const [desc, setDesc] = useState({desc: '', date: ''}); 
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setDesc({ ...desc, [event.target.name]: event.target.value });
+  }
+
+  const addToDo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, desc]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={addToDo}>
+        <p>Date</p>
+        <input type="date" name="date" value={desc.date} onChange={inputChanged} />
+        <p>Description</p>
+        <input type="text" name="desc" value={desc.desc} onChange={inputChanged} />
+        <input type="submit" value="Add"/>
+      </form>
+      <Todotable todos={todos} />
+     
+     
     </div>
   );
 }
